22c6583e8f0e1f0c42fe0c6c956884792131a727
WiLD11 <florinel1058@gmail.com>
2017-07-29 22:15:12 +0200

    planets placed from level data in defs file

--- js/states/Game_create.js
+++ js/states/Game_create.js
@@ -8,21 +8,21 @@ GameState.prototype.create = function() {
 
   this.game.add.image(0, 0, 'background');
 
-  g_game.planet1 = this.game.add.sprite(100, 100, 'planet1');
+  g_game.planet1 = this.game.add.sprite(g_game.levels[g_game.currentlvl].planet1.x,g_game.levels[g_game.currentlvl].planet1.y, g_game.levels[g_game.currentlvl].planet1.key);
   g_game.planet1.anchor.setTo(0.5, 0.5);
   g_game.planet1.customProps = {
     orbitDiameter : 32
   };
   drawOrbit(this.game, g_game.planet1);
 
-  g_game.planet2 = this.game.add.sprite(200, 300, 'planet2');
+  g_game.planet2 = this.game.add.sprite(g_game.levels[g_game.currentlvl].planet2.x,g_game.levels[g_game.currentlvl].planet2.y, g_game.levels[g_game.currentlvl].planet2.key);
   g_game.planet2.anchor.setTo(0.5, 0.5);
   g_game.planet2.customProps = {
     orbitDiameter : 64
   };
   drawOrbit(this.game, g_game.planet2);
 
-  g_game.planet3 = this.game.add.sprite(400, 100, 'planet1');
+  g_game.planet3 = this.game.add.sprite(g_game.levels[g_game.currentlvl].planet3.x,g_game.levels[g_game.currentlvl].planet3.y, g_game.levels[g_game.currentlvl].planet3.key);
   g_game.planet3.anchor.setTo(0.5, 0.5);
   g_game.planet3.customProps = {
     orbitDiameter : 32
