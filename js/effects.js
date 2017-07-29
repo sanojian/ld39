3e669a9968d73f9cad274f03516fe8331b38bfe4
WiLD11 <florinel1058@gmail.com>
2017-07-29 15:56:15 +0200

    outlines for orbiting diameters

--- js/effects.js
+++ js/effects.js
@@ -1,3 +1,6 @@
-function drawOrbit(planet) {
+function drawOrbit(game,planet) {
+    var graphics = game.add.graphics(planet.x,planet.y);
+  graphics.lineStyle(2, 0x0000FF, 1);
+    graphics.drawCircle(0,0, planet.customProps.orbitDiameter * 2);
 
 }
