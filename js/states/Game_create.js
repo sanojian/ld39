6593346985ee4ee00c9263ac7fa7eb3ef164ac8d
Jonas Olmstead <sanojo@hotmail.com>
2017-07-29 15:11:44 +0200

    fixed typo

--- js/states/Game_create.js
+++ js/states/Game_create.js
@@ -11,14 +11,14 @@ GameState.prototype.create = function() {
   g_game.planet1.customProps = {
     orbitDiameter : 32
   };
-  drawOrbit(planet1);
+  drawOrbit(g_game.planet1);
 
   g_game.planet2 = this.game.add.sprite(200, 300, 'planet2');
   g_game.planet2.anchor.setTo(0.5, 0.5);
   g_game.planet2.customProps = {
     orbitDiameter : 64
   };
-  drawOrbit(planet2);
+  drawOrbit(g_game.planet2);
 
   var player = this.game.add.sprite(0, 0, 'ship1');
   player.anchor.setTo(0.5, 0.5);
