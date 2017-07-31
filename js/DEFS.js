17e4b10f00160bbb8d4f35f485e357932adc323f
WiLD11 <florinel1058@gmail.com>
2017-07-31 22:51:58 +0200

    level 8+9

--- js/DEFS.js
+++ js/DEFS.js
@@ -10,12 +10,13 @@ window.g_game = {
     planets: [],
     asteroids: [],
     specialAsteroids: [],
-    editor: true,
-    currentlvl: 6,
+    editor: false,
+    currentlvl: 9,
+    maxlvl: 9,
     levels: {
         //done
         1: {
-            planet1: { x: 66, y: 226, key: 'planet1', scale: 1, type: 'planet' },
+            planet1: { x: 66, y: 226, key: 'planet1', scale: 1, type: 'planet', isStart: true },
             planet2: { x: 324, y: 226, key: 'planet2', scale: 1, isFuel: true, type: 'planet' },
             planet3: { x: 578, y: 235, key: 'planet3', scale: 1, isGoal: true, type: 'planet' },
             message: 'Good job \n BUT, SHE IS ON ANOTHER PLANET!'
@@ -23,7 +24,7 @@ window.g_game = {
 
         //done
         2: {
-            planet1: { x: 66, y: 226, key: 'planet4', scale: 1, type: 'planet' },
+            planet1: { x: 66, y: 226, key: 'planet4', scale: 1, type: 'planet', isStart: true },
             planet2: { x: 324, y: 226, key: 'planet3', scale: 1, isFuel: true, type: 'planet' },
             planet3: { x: 578, y: 235, key: 'planet2', scale: 1, isGoal: true, type: 'planet' },
             asteroid1: { x: 170, y: 159, key: 'asteroid1', scale: 2, type: 'asteroid' },
@@ -32,15 +33,15 @@ window.g_game = {
         },
         //done
         3: {
-            planet1: { x: 66, y: 226, key: 'planet3', scale: 1, type: 'planet' },
+            planet1: { x: 66, y: 226, key: 'planet3', scale: 1, type: 'planet', isStart: true },
             planet2: { x: 578, y: 235, key: 'planet1', scale: 1, isGoal: true, type: 'planet' },
             asteroid1: { x: 324, y: 226, key: 'asteroid2', scale: 0.8, special: true, type: 'asteroid' },
-            message: 'Our princess is in another cas...\n I mean, she is on another planet!'
+            message: 'Our princess is in another cas...\n I mean, on another planet!'
 
         },
         //done
         4: {
-            planet1: { x: 66, y: 226, key: 'planet1', scale: 1, type: 'planet' },
+            planet1: { x: 66, y: 226, key: 'planet1', scale: 1, type: 'planet', isStart: true },
             planet2: { x: 578, y: 235, key: 'planet3', scale: 1, isGoal: true, type: 'planet' },
             asteroid1: { x: 324, y: 324, key: 'asteroid2', scale: 0.8, special: true, type: 'asteroid' },
             asteroid2: { x: 324, y: 226, key: 'asteroid1', scale: 2, type: 'asteroid' },
@@ -48,7 +49,7 @@ window.g_game = {
 
         },
         5: {
-            planet1: { x: 130, y: 371, key: 'planet3', scale: 1, type: 'planet' },
+            planet1: { x: 130, y: 371, key: 'planet3', scale: 1, type: 'planet', isStart: true },
             planet2: { x: 412, y: 64, key: 'planet2', scale: 1, type: 'planet', isGoal: true },
 
             asteroid1: { x: 10, y: 250, key: 'asteroid1', scale: 1, special: false, type: 'asteroid' },
@@ -83,7 +84,7 @@ window.g_game = {
             message: 'Good job \n BUT, SHE IS ON ANOTHER PLANET!'
         },
         6: {
-            planet1: { x: 100, y: 128, key: 'planet1', scale: 1, type: 'planet' },
+            planet1: { x: 100, y: 128, key: 'planet1', scale: 1, type: 'planet', isStart: true },
             planet2: { x: 450, y: 128, key: 'planet1', scale: 1, type: 'planet' },
             asteroid1: { x: 184, y: 60, key: 'asteroid1', scale: 1, special: false, type: 'asteroid' },
             asteroid2: { x: 195, y: 75, key: 'asteroid1', scale: 1, special: false, type: 'asteroid' },
@@ -101,7 +102,7 @@ window.g_game = {
 
 
         7: {
-            planet1: { x: 534, y: 76, key: 'planet2', scale: 1, type: 'planet' },
+            planet1: { x: 534, y: 76, key: 'planet2', scale: 1, type: 'planet', isStart: true },
             planet2: { x: 535, y: 192, key: 'planet3', scale: 1, type: 'planet' },
             planet3: { x: 535, y: 300, key: 'planet1', scale: 1, type: 'planet' },
             asteroid1: { x: 481, y: 375, key: 'asteroid2', scale: 1, special: true, type: 'asteroid' },
@@ -152,6 +153,125 @@ window.g_game = {
 
         },
 
+        8: {
+      planet1: { x: 288, y: 312, key: 'planet1', scale: 1, isFuel: true, type: 'planet', isStart: true},
+            planet2: { x: 264, y: 120, key: 'planet1', scale: 1, isFuel: true, type: 'planet' },
+            planet3: { x: 384, y: 96+24, key: 'planet1', scale: 1, isFuel: true, type: 'planet' },
+         planet4: { x: 480, y: 360, key: 'planet1', scale: 1, isFuel: true, type: 'planet', isGoal: true },
+            asteroid1: { x: 384, y: 408, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid2: { x: 384, y: 384, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid3: { x: 384, y: 360, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid4: { x: 384, y: 336, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid5: { x: 384, y: 312, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid6: { x: 384, y: 264, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid7: { x: 384, y: 288, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid8: { x: 384, y: 240, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid9: { x: 384, y: 216, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid10: { x: 264, y: 216, key: 'asteroid1', scale: 1.2, special: true, type: 'asteroid' },
+            asteroid11: { x: 240, y: 216, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid12: { x: 216, y: 216, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid13: { x: 192, y: 216, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid14: { x: 168, y: 216, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid15: { x: 144, y: 216, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid16: { x: 120, y: 216, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid17: { x: 96, y: 216, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid18: { x: 72, y: 216, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid19: { x: 48, y: 216, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid20: { x: 24, y: 216, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid21: { x: 168, y: 192, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid22: { x: 168, y: 168, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid23: { x: 168, y: 144, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid24: { x: 168, y: 120, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid25: { x: 168, y: 96, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid26: { x: 168, y: 72, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid27: { x: 168, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid28: { x: 192, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid29: { x: 216, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid30: { x: 240, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid31: { x: 264, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid32: { x: 288, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid33: { x: 312, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid34: { x: 336, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid35: { x: 360, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid36: { x: 384, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid37: { x: 408, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid38: { x: 432, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid39: { x: 480, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid40: { x: 456, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid41: { x: 504, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid42: { x: 384, y: 192, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid43: { x: 384, y: 168, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid45: { x: 528, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid46: { x: 552, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid47: { x: 552, y: 72, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid48: { x: 552, y: 96, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid49: { x: 552, y: 120, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid50: { x: 552, y: 144, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid51: { x: 552, y: 168, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid52: { x: 552, y: 192, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid53: { x: 552, y: 216, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid54: { x: 552, y: 240, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid55: { x: 552, y: 264, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid56: { x: 552, y: 288, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid57: { x: 552, y: 312, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid58: { x: 552, y: 336, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid59: { x: 552, y: 360, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid60: { x: 552, y: 384, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid61: { x: 552, y: 408, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+      
+            asteroid62: { x: 528, y: 192, key: 'asteroid1', scale: 1.2, special: true, type: 'asteroid' },
+            asteroid63: { x: 408, y: 288, key: 'asteroid1', scale: 1.2, special: true, type: 'asteroid' },
+   
+        },
+
+        9: {
+            asteroid1: { x: 552, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid2: { x: 552, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid3: { x: 480, y: 120, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid4: { x: 456, y: 144, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid5: { x: 432, y: 168, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid6: { x: 240, y: 168, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid7: { x: 216, y: 144, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid8: { x: 192, y: 120, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid9: { x: 120, y: 48, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid10: { x: 240, y: 264, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid11: { x: 432, y: 264, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid12: { x: 264, y: 264, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid13: { x: 288, y: 264, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid14: { x: 312, y: 264, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid15: { x: 336, y: 264, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid16: { x: 360, y: 264, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid17: { x: 384, y: 264, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid18: { x: 408, y: 264, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid19: { x: 408, y: 168, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid20: { x: 384, y: 168, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid21: { x: 360, y: 168, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid22: { x: 336, y: 168, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid23: { x: 312, y: 168, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid24: { x: 288, y: 168, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid25: { x: 264, y: 168, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid26: { x: 432, y: 192, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid27: { x: 432, y: 216, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid28: { x: 432, y: 240, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid29: { x: 216, y: 288, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid30: { x: 192, y: 312, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid31: { x: 120, y: 384, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid32: { x: 456, y: 288, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid33: { x: 480, y: 312, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid34: { x: 552, y: 384, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            planet1: { x: 192, y: 216, key: 'planet1', scale: 1, isFuel: true,isStart: true, type: 'planet' },
+            asteroid35: { x: 168, y: 336, key: 'asteroid1', scale: 1.2, special: true, type: 'asteroid' },
+            asteroid36: { x: 504, y: 336, key: 'asteroid1', scale: 1.2, special: true, type: 'asteroid' },
+            asteroid37: { x: 504, y: 96, key: 'asteroid1', scale: 1.2, special: true, type: 'asteroid' },
+            planet2: { x: 336, y: 384, key: 'planet1', scale: 1, isFuel: true, type: 'planet' },
+            planet3: { x: 552, y: 216, key: 'planet1', scale: 1, isFuel: true, type: 'planet' },
+            planet4: { x: 336, y: 72, key: 'planet1', scale: 1, isFuel: true, type: 'planet' },
+            asteroid38: { x: 144, y: 72, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            asteroid39: { x: 168, y: 96, key: 'asteroid1', scale: 1.2, special: false, type: 'asteroid' },
+            planet5: { x: 336, y: 216, key: 'planet1', scale: 1, isFuel: true, type: 'planet', isGoal: true },
+
+
+        },
 
 
     }
